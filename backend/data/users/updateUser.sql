861dab9217fd16cec39ed485ca53c8066fd6762b
BlerionMuco <66955735+BlerionMuco@users.noreply.github.com>
2022-12-27 22:52:30 +0100

    Add columns for user

--- backend/data/users/updateUser.sql
+++ backend/data/users/updateUser.sql
@@ -1,12 +1,23 @@
 UPDATE [dbo].[users]
 SET [first_name]=@firstName,
     [last_name]=@lastName,
-    [age]=@age
+    [age]=@age,
+    [username]=@username,
+    [email]=@email,
+    [phone_number]=@phone_number,
+    [user_password]=@user_password,
+    [user_verification_password]=@user_verification_password,
+    [role_id]=@role_id,
 WHERE [id]=@userId
 
 SELECT [id]
       ,[first_name]
       ,[last_name]
       ,[age]
+      ,[username]
+      ,[phone_number]
+      ,[user_password]
+      ,[user_verification_password]
+      ,[role_id]
   FROM [dbo].[users]
   WHERE [id]=@userId
