297e08126edb3fefa8a6d6e7664e7afc542c1fdc
Blerion Muco <blerion.muco3@gmail.com>
2023-01-08 12:14:51 +0000

    fix checkValidation for token

--- frontend/src/Containers/Nav/WithDashboard.js
+++ frontend/src/Containers/Nav/WithDashboard.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import DashboardComponent from '../Components/DashboardComponent'
+import { Navigate } from 'react-router-dom'
 
 const WithDashboard = ({component}) => {
+  const checkValidToken = () => {
+    const token = localStorage.getItem('token');
+    if (token) { return true } else { return false; }
+  }
   return (
-    <div><DashboardComponent component={component} /></div>
+    <div>{checkValidToken() ? <DashboardComponent component={component} /> : <Navigate to="/" /> }</div>
   )
 }
 
