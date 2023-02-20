eda478a4e861e82a0d527aa4d737b78e104713cc
Blerion Muco <blerion.muco3@gmail.com>
2023-02-20 11:51:54 +0100

    Format Code

--- frontend/src/Components/User/NewUserComponent.jsx
+++ frontend/src/Components/User/NewUserComponent.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import TextField from "@mui/material/TextField";
 import Grid from "@mui/material/Grid";
+import { newUser } from "../../Contains/User/NewUser";
 
 const NewUserComponent = ({ dataForm, setDataForm }) => {
   const handleChange = (e) => {
@@ -20,101 +21,21 @@ const NewUserComponent = ({ dataForm, setDataForm }) => {
         </Grid>
         <Grid item xs={12}>
           <Grid container spacing={2}>
-            <Grid item xs={6}>
-              <TextField
-                onChange={handleChange}
-                name="first_name"
-                size="small"
-                required
-                label="Firstname"
-                sx={{ width: "100%" }}
-                value={dataForm.first_name}
-              />
-            </Grid>
-            <Grid item xs={6}>
-              <TextField
-                onChange={handleChange}
-                name="last_name"
-                size="small"
-                required
-                label="Lastname"
-                sx={{ width: "100%" }}
-                value={dataForm.last_name}
-              />
-            </Grid>
+            {newUser.length > 0 &&
+              newUser.map((userField, index) => (
+                <Grid key={index} item xs={userField.xs}>
+                  <TextField
+                    onChange={handleChange}
+                    name={userField.name}
+                    size={userField.size}
+                    required={userField.required}
+                    label={userField.label}
+                    sx={{ width: "100%" }}
+                    value={dataForm[userField.name]}
+                  />
+                </Grid>
+              ))}
           </Grid>
-        </Grid>
-        <Grid item xs={12}>
-          <Grid container spacing={2}>
-            <Grid item xs={6}>
-              <TextField
-                onChange={handleChange}
-                name="age"
-                size="small"
-                label="Age"
-                sx={{ width: "100%" }}
-                value={dataForm.age}
-              />
-            </Grid>
-            <Grid item xs={6}>
-              <TextField
-                onChange={handleChange}
-                name="phone_number"
-                size="small"
-                label="Phone number"
-                sx={{ width: "100%" }}
-                value={dataForm.phone_number}
-              />
-            </Grid>
-          </Grid>
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            onChange={handleChange}
-            name="username"
-            size="small"
-            required
-            label="Username"
-            sx={{ width: "100%" }}
-            value={dataForm.username}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            onChange={handleChange}
-            name="email"
-            size="small"
-            required
-            label="Email"
-            sx={{ width: "100%" }}
-            value={dataForm.email}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            onChange={handleChange}
-            name="user_password"
-            sx={{ width: "100%" }}
-            size="small"
-            required
-            label="Password"
-            type="password"
-            autoComplete="new-password"
-            value={dataForm.user_password}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            onChange={handleChange}
-            name="user_verification_password"
-            sx={{ width: "100%" }}
-            size="small"
-            required
-            label="Verify Password"
-            type="password"
-            autoComplete="new-password"
-            value={dataForm.user_verification_password}
-          />
         </Grid>
       </Grid>
     </div>
